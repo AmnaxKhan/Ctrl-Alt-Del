608ffa92c626a475428bcf1b9ac0646b7c91c8ff
amnakhan <akaykkhan@gmail.com>
2024-04-13 09:20:13 -0500

    excercises draft

---
+++ front_end/main.js
@@ -0,0 +1,9 @@
+let angle = 90; 
+
+if (angle > 0 && angle < 90) {
+    console.log("do this excercise!");
+  } else if (x > 90 && x < 180) {
+    console.log("do this excercise!");
+  } else {
+    console.log("Not possible");
+  }
